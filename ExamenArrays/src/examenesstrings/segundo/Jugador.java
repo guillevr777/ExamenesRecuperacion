df3c44a2cacf157e753446d48a814434df401a03
guillevr777 <gs.villanueva@iesnervion.es>
2025-06-04 17:58:17 +0200

    l

--- ExamenArrays/src/examenesstrings/segundo/Jugador.java
+++ ExamenArrays/src/examenesstrings/segundo/Jugador.java
@@ -1,8 +1,59 @@
 package examenesstrings.segundo;
 
+import java.util.Random;
+
 public class Jugador {
 
 	String juegos[][] = {{"CASA","PATO"},{"PISO","TUBO"},{"CENA","MURO"},{"BONO","DEDO"},{"ROJO","PASA"}};
-
 	
+	final int NUMINTENTOS = 7;
+
+	String palabraInicio = "";
+	
+	String palabraObjetivo = "";
+	 
+	String jugada = "";
+	
+	public void seleccionaJuego () {
+		Random random = new Random();
+		int fila = random.nextInt(0,juegos.length);
+		
+		palabraInicio = juegos[fila][0];
+		palabraObjetivo = juegos[fila][1];
+		jugada = palabraInicio;
+	}
+	public String ultimoIntento() {
+		String palabra = jugada;
+		int posicion;
+		
+		posicion = jugada.lastIndexOf(" ");
+		
+		if (posicion >= 0) {
+			
+ 		palabra = jugada.substring(posicion + 1);
+ 		
+		}
+		
+		return palabra;
+	}
+	public boolean compruebaIntento(String intento) {
+		boolean exito = false;
+		String palabra = ultimoIntento(); 
+		char palabraDesglozada[] = palabra.toUpperCase().toCharArray();
+		char intentoDesglozado[] = intento.toUpperCase().toCharArray();
+		int contador = 0;
+		
+			for (int i = 0 ; i < palabra.length() ; i++) {
+				if (palabraDesglozada[i] != intentoDesglozado[i]) {
+					contador++;
+				}
+			}
+			
+			if (contador == 1) {
+				exito = true;
+				jugada += " - " + intento.toUpperCase();
+			}
+		
+		return exito;
+	}
 }
--- ExamenArrays/src/examenesstrings/segundo/Principal.java
+++ ExamenArrays/src/examenesstrings/segundo/Principal.java
@@ -1,9 +1,44 @@
 package examenesstrings.segundo;
 
+import java.util.Scanner;
+
 public class Principal {
 
 	public static void main(String[] args) {
 
+		Scanner reader = new Scanner(System.in);
+		Jugador jugador = new Jugador ();
+		String palabra = "";
+		int contador = jugador.NUMINTENTOS;
 		
+		jugador.seleccionaJuego();
+		System.out.println("palabraInicio: " + jugador.palabraInicio + " - palabraObjetivo: " + jugador.palabraObjetivo);
+		
+		do {			
+		do {
+		try {
+		System.out.println("Dime una palabra a probar:");
+		palabra = reader.nextLine();
+		assert (palabra.length() == 4) : "La cadena tiene que tener una longitud de 4 caracteres variando solo 1 con la palabra anterior.";
+		} catch (AssertionError e) {
+			System.out.println(e);
+		}
+		} while (palabra.length() != 4);		
+		
+		if (!jugador.compruebaIntento(palabra)) {
+			System.out.println("Cometiste un error o cambiaste mas de 1 letra simultanea en el mismo turno.");
+		} else {
+			contador--;
+		}
+		
+		System.out.println("Jugada: " + jugador.jugada);
+
+		} while (contador > 0 && !jugador.ultimoIntento().equalsIgnoreCase(jugador.palabraObjetivo));
+		
+		if (contador > 0) {
+			System.out.println("Ganaste!");
+		} else {
+			System.out.println("Perdiste!");
+		}
 	}
 }
